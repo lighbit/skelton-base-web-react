49bde42b0fbf5dee0af0658bc8ead0dd331f209e
zulkarnaen <sekaizulka.sz@gmail.com>
2021-09-08 18:00:30 +0700

    inital

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import $ from 'jquery';
+
+window.jQuery = $;
+window.$ = $;
+global.jQuery = $;
+
+const OtherSamplePage = React.lazy(() => import('./Demo/Other/SamplePage'));
+
+const routes = [
+    { path: '/sample-page', exact: true, name: 'Sample Page', component: OtherSamplePage },
+    { path: '/coba-page', exact: true, name: 'Ini ada page', component: OtherSamplePage },
+];
+
+export default routes;
